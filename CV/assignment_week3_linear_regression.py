119a25962670ff5b32cc329f2c6c9deab9d84594
scarletlx <4875648@qq.com>
2019-07-16 14:38:50 +0800

    Add files via upload

--- CV/assignment_week3_linear_regression.py
+++ CV/assignment_week3_linear_regression.py
@@ -4,6 +4,7 @@
 ###############################
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 # X: (100, 1)
 # y: (100, 1)
@@ -16,7 +17,7 @@ def cal_step_gradient(batch_x, batch_y, w, b, lr):
     diff = y_pred - batch_y
     loss = np.dot(diff.T, diff) / (2 * m)
 
-    dw = np.dot(diff.T, batch_x)/m
+    dw = np.sum(np.dot(diff.T, batch_x), axis=0)/m
     db = (1.0 / m) * np.sum(diff)
     w -= lr * dw
     b -= lr * db
@@ -24,9 +25,10 @@ def cal_step_gradient(batch_x, batch_y, w, b, lr):
 
 
 def train(X, y, batch_size, lr, max_iter):
-    w = random.randint(0, 10)
-    b = random.randint(0, 5)
+    w = np.zeros(X.shape[1])
+    b = 0
     num_samples = X.shape[0]
+    print("w shape = ", w.shape)
     for i in range(max_iter):
         batch_idxs = np.random.choice(num_samples, batch_size)
         batch_x = X[batch_idxs]
@@ -34,13 +36,21 @@ def train(X, y, batch_size, lr, max_iter):
         loss, w, b = cal_step_gradient(batch_x, batch_y, w, b, lr)
         print('iteration: {0}, w:{1}, b:{2}, loss is {3}'.format(i, w, b, loss))
 
+    y_pred = np.dot(X, w) + b
+    plt.plot(X, y_pred, 'r', label='Prediction')
+    plt.scatter(X, y, marker=".", label="Training Data")
+    plt.xlabel("X")
+    plt.ylabel("y")
+    plt.legend(loc=2)
+    plt.show()
+
 
 def gen_sample_data():
-    w = random.randint(0, 10) + random.random()		# for noise random.random[0, 1)
-    b = random.randint(0, 5) + random.random()
     num_samples = 100
-    X = np.random.randint(0, 100, (num_samples, 1)) * random.random()
-    y = np.dot(X, w) + b + random.random() * random.randint(-1, 1)
+    X = np.random.rand(num_samples, 1) * 10
+    w = np.random.rand(X.shape[1]) 		# for noise random.random[0, 1)
+    b = 2
+    y = np.dot(X, w) + b + np.random.randn(num_samples,)* 0.2
     return X, y, w, b
 
 
